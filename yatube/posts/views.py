1ba5e4cb958b836eb18d3a673858f48c67914634
Анастасия Долгова <a3894685@yandex.ru>
2022-09-02 14:42:49 +0300

    Правки 3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -36,8 +36,7 @@ def group_posts(request, slug):
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     page_obj = get_page(request, author.posts.all().order_by(
-        'group', '-pub_date')
-        )
+        'group', '-pub_date'))
     context = {
         'author': author,
         'page_obj': page_obj,
